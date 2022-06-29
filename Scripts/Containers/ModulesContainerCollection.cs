fa87541be2cdd7b8903db15d4e95f8999084ae02
kirisakiken <bezmican96@gmail.com>
2022-06-29 19:47:42 +0300

    feat: add existing files from phoenix networking project
    add GameSystem
    add Modules
    add Providers
    add Tools

---
+++ Scripts/Containers/ModulesContainerCollection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+using KirisakiTechnologies.GameSystem.Scripts.Modules;
+
+using UnityEngine;
+
+namespace KirisakiTechnologies.GameSystem.Scripts.Containers
+{
+    public class ModulesContainerCollection : MonoBehaviour, IModulesContainerCollection
+    {
+        #region IModulesContainerCollection Implementation
+
+        public IReadOnlyCollection<IGameModule> Modules => GetComponentsInChildren<IGameModule>(false);
+
+        #endregion
+    }
+}
