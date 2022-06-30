20d9ee86b8547b6a179bea3f7cfae618658a128d
Bezmican Zehir <bezmican@aifi.com>
2022-06-30 22:28:24 +0300

    feat: add entities base module and provider

---
+++ Scripts/Modules/Entities/EntitiesModule.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using KirisakiTechnologies.GameSystem.Scripts.Entities;
+using KirisakiTechnologies.GameSystem.Scripts.Extensions;
+using KirisakiTechnologies.GameSystem.Scripts.Providers.Entities;
+
+namespace KirisakiTechnologies.GameSystem.Scripts.Modules.Entities
+{
+    public class EntitiesModule : GameModuleBaseMono, IEntitiesModule
+    {
+        #region IEntitiesModule Implementation
+
+        public IReadOnlyList<IEntity> Entities => _Entities;
+
+        #endregion
+
+        #region Overrides
+
+        public override async Task Begin(IGameSystem gameSystem)
+        {
+            _EntitiesProvider = gameSystem.GetProvider<IEntitiesProvider>();
+            _Entities.AddRange(await _EntitiesProvider.LoadEntities());
+
+            await base.Begin(gameSystem);
+        }
+
+        #endregion
+
+        #region Private
+
+        private readonly List<IEntity> _Entities = new List<IEntity>();
+
+        private IEntitiesProvider _EntitiesProvider;
+
+        #endregion
+    }
+}
---
+++ Scripts/Providers/Entities/IEntitiesProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using KirisakiTechnologies.GameSystem.Scripts.Entities;
+
+namespace KirisakiTechnologies.GameSystem.Scripts.Providers.Entities
+{
+    /// <summary>
+    ///     Responsible of providing entities
+    /// </summary>
+    public interface IEntitiesProvider : IGameProvider
+    {
+        /// <summary>
+        ///     Loads entities
+        /// </summary>
+        /// <returns></returns>
+        Task<List<IEntity>> LoadEntities();
+    }
+}
