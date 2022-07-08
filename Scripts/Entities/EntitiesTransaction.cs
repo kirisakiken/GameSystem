bb237216b12214433a8100ead0125c69798e8775
Bezmican Zehir <bezmican@aifi.com>
2022-07-08 23:55:49 +0300

    refactor(entities): refactor entities base module

---
+++ Scripts/Entities/EntitiesTransaction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace KirisakiTechnologies.GameSystem.Scripts.Entities
+{
+    /// <summary>
+    ///     Represents transaction of entities e.g.
+    ///     Added, Modified and Removed entities
+    /// </summary>
+    public class EntitiesTransaction
+    {
+        #region Public
+
+        public List<IEntity> AddedEntities { get; } = new List<IEntity>();
+        public List<IEntity> ModifiedEntities { get; } = new List<IEntity>();
+        public List<IEntity> RemovedEntities { get; } = new List<IEntity>();
+
+        #endregion
+    }
+}
--- Scripts/Modules/Entities/IEntitiesModule.cs
+++ Scripts/Modules/Entities/IEntitiesModule.cs
@@ -1,17 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System.Threading.Tasks;
 
 using KirisakiTechnologies.GameSystem.Scripts.Entities;
 
 namespace KirisakiTechnologies.GameSystem.Scripts.Modules.Entities
 {
+    public delegate void OnEntitiesChanged(IEntity entity);
+
     /// <summary>
     ///     Holds ownership of entities
     /// </summary>
     public interface IEntitiesModule : IGameModule
     {
         /// <summary>
-        ///     Collection of entities
+        ///     Invoked whenever an entity added, modified or removed
         /// </summary>
-        IReadOnlyList<IEntity> Entities { get; }
+        event OnEntitiesChanged OnEntitiesChanged;
+
+        /// <summary>
+        ///     Returns requested entity by id
+        ///     returns null if not found
+        /// </summary>
+        T GetEntity<T>(int id) where T : IEntity;
+
+        /// <summary>
+        ///     Returns next available entity id
+        ///     (max + 1)
+        /// </summary>
+        /// <returns></returns>
+        int GetNextEntityId();
+
+        /// <summary>
+        ///     Updates entities with given transaction
+        /// </summary>
+        Task UpdateEntities(EntitiesTransaction transaction);
     }
 }
