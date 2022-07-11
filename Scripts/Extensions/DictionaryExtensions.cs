2a6adff1ee245102c2894f6d858ba6b81dcdddc6
kirisakiken <bezmican96@gmail.com>
2022-07-12 00:35:31 +0300

    feat(extensions): add dictionary extensions

---
+++ Scripts/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KirisakiTechnologies.GameSystem.Scripts.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static void UpdateAll<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TValue value)
+        {
+            var keys = dictionary.Keys.ToList();
+            foreach (var key in keys)
+                dictionary[key] = value;
+        }
+
+        public static void UpdateAll<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, Func<TValue, TValue> updatePredicate)
+        {
+            var keys = dictionary.Keys.ToList();
+            foreach (var key in keys)
+                dictionary[key] = updatePredicate(dictionary[key]);
+        }
+
+        public static void UpdateAll<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, Func<TKey, TValue, bool> matchPredicate, Func<TValue, TValue> updatePredicate)
+        {
+            var keys = dictionary.Keys.Where(k => matchPredicate(k, dictionary[k])).ToList();
+            foreach (var key in keys)
+                dictionary[key] = updatePredicate(dictionary[key]);
+        }
+    }
+}
