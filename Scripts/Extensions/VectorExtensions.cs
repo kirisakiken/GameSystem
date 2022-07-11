8ea127dc2a6ce2fa332615f1ce48d614e2ed4c6b
kirisakiken <bezmican96@gmail.com>
2022-07-11 21:47:33 +0300

    feat(extensions): add vector extensions

---
+++ Scripts/Extensions/VectorExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace KirisakiTechnologies.GameSystem.Scripts.Extensions
+{
+    public static class VectorExtensions
+    {
+        public static bool Approximately(Vector3 a, Vector3 b)
+        {
+            return Mathf.Approximately(a.x, b.x) && Mathf.Approximately(a.y, b.y) && Mathf.Approximately(a.z, b.z);
+        }
+
+        public static bool IsZero(this Vector3 self)
+        {
+            return Approximately(self, Vector3.zero);
+        }
+    }
+}
